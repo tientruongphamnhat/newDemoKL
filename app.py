6e7f06e3d9976778ac6cdc8f29923b8a1699c00e
tientruongphamnhat <49446415+tientruongphamnhat@users.noreply.github.com>
2020-07-20 00:37:15 +0700

    update vocab and app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 import nltk
 from nltk import word_tokenize
 import collections
@@ -12,8 +14,9 @@ import numpy as np
 from gensim.models import KeyedVectors
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
+import kenlm
+model = kenlm.Model('vietnamese_wiki_3-gram.binary')
+
 
 app = Flask(__name__)
 CORS(app)
@@ -27,24 +30,6 @@ word2int_english_path = "vocab_english/word2int.pickle"
 int2word_english_path = "vocab_english/int2word.pickle"
 word2int_vietnamese_path = "vocab_vietnamese/word2int.pickle"
 int2word_vietnamese_path = "vocab_vietnamese/int2word.pickle"
-
-
-def getData(path):
-    sentList = []
-    with open(path) as f:
-        # using 100 to test the code only
-        for line in f.readlines():
-            w = line.lower()
-            w = re.sub(r"([?.!,Ã‚Â¿])", r" \1 ", w)
-            w = re.sub(r'[" "]+', " ", w)
-            #w = re.sub(r"[^a-zA-Z?.!,Ã‚Â¿]+", " ", w)
-            w = w.strip()
-
-            # Add sent to the list
-            sentList.append(w)
-
-    return sentList
-
 
 # Load vocab dictionary word2int, int2word
 with open(word2int_english_path, 'rb') as fopen:
@@ -193,7 +178,136 @@ def get_batches(input_data, output_data, batch_size):
         yield input_data[start_index:end_index], output_data[start_index:end_index]
 
 
+#CNN + GLU
+def position_encoding(sentence_size, embedding_size):
+    encoding = np.ones((sentence_size, embedding_size), dtype=np.float32)
+    ls = sentence_size + 1
+    le = embedding_size + 1
+    for k in range(1, le):
+        for j in range(1, ls):
+            encoding[j-1, k-1] = (1.0 - j/float(ls)) - (
+                k / float(le)) * (1. - 2. * j/float(ls))
+
+    return encoding
+
+
+def _create_position_embedding(embedding_dim, num_positions, lengths, maxlen):
+    # Create constant position encodings
+    position_encodings = tf.constant(
+        position_encoding(num_positions, embedding_dim))
+
+    # Slice to size of current sequence
+    pe_slice = position_encodings[:maxlen, :]
+    # Replicate encodings for each element in the batch
+    batch_size = tf.shape(lengths)[0]
+    pe_batch = tf.tile([pe_slice], [batch_size, 1, 1])
+
+    # Mask out positions that are padded
+    positions_mask = tf.sequence_mask(
+        lengths=lengths, maxlen=maxlen, dtype=tf.float32)
+    positions_embed = pe_batch * tf.expand_dims(positions_mask, 2)
+
+    return positions_embed
+
+# CNN layer
+
+
+# padding should take attention
+def conv1d_weightnorm(inputs, layer_idx, out_dim, kernel_size, padding="SAME", dropout=1.0,  var_scope_name="conv_layer"):
+    with tf.variable_scope("conv_layer_"+str(layer_idx)):
+        in_dim = int(inputs.get_shape()[-1])
+        V = tf.get_variable('V', shape=[kernel_size, in_dim, out_dim], dtype=tf.float32, initializer=tf.random_normal_initializer(
+            mean=0, stddev=tf.sqrt(4.0*dropout/(kernel_size*in_dim))), trainable=True)
+        # V shape is M*N*k,  V_norm shape is k
+        V_norm = tf.norm(V.initialized_value(), axis=[0, 1])
+        g = tf.get_variable('g', dtype=tf.float32,
+                            initializer=V_norm, trainable=True)
+        b = tf.get_variable('b', shape=[
+                            out_dim], dtype=tf.float32, initializer=tf.zeros_initializer(), trainable=True)
+
+        # use weight normalization (Salimans & Kingma, 2016)
+        W = tf.reshape(g, [1, 1, out_dim])*tf.nn.l2_normalize(V, [0, 1])
+        inputs = tf.nn.bias_add(tf.nn.conv1d(
+            value=inputs, filters=W, stride=1, padding=padding), b)
+        return inputs
+
+# tang kich thuoc layer truoc khi vao cnn dam bao shape ko thay doi, so luong du lieu van duoc giu lai
+
+
+def linear_mapping_weightnorm(inputs, out_dim, in_dim=None, dropout=1.0, var_scope_name="linear_mapping"):
+    with tf.variable_scope(var_scope_name):
+        input_shape = inputs.get_shape().as_list()    # static shape. may has None
+        input_shape_tensor = tf.shape(inputs)
+        # use weight normalization (Salimans & Kingma, 2016)  w = g* v/2-norm(v)
+        V = tf.get_variable('V', shape=[int(input_shape[-1]), out_dim], dtype=tf.float32, initializer=tf.random_normal_initializer(
+            mean=0, stddev=tf.sqrt(dropout*1.0/int(input_shape[-1]))), trainable=True)
+        # V shape is M*N,  V_norm shape is N
+        V_norm = tf.norm(V.initialized_value(), axis=0)
+        g = tf.get_variable('g', dtype=tf.float32,
+                            initializer=V_norm, trainable=True)
+        b = tf.get_variable('b', shape=[out_dim], dtype=tf.float32, initializer=tf.zeros_initializer(
+        ), trainable=True)   # weightnorm bias is init zero
+
+        assert len(input_shape) == 3
+        inputs = tf.reshape(inputs, [-1, input_shape[-1]])
+        inputs = tf.matmul(inputs, V)
+        inputs = tf.reshape(inputs, [input_shape_tensor[0], -1, out_dim])
+        # inputs = tf.matmul(inputs, V)    # x*v
+
+        scaler = tf.div(g, tf.norm(V, axis=0))   # g/2-norm(v)
+        # x*v g/2-norm(v) + b
+        inputs = tf.reshape(scaler, [1, out_dim]) * \
+            inputs + tf.reshape(b, [1, out_dim])
+
+        return inputs
+
+
+def position_encoding(inputs):
+    T = tf.shape(inputs)[1]
+    repr_dim = inputs.get_shape()[-1].value
+    pos = tf.reshape(tf.range(0.0, tf.to_float(T), dtype=tf.float32), [-1, 1])
+    i = np.arange(0, repr_dim, 2, np.float32)
+    denom = np.reshape(np.power(10000.0, i / repr_dim), [1, -1])
+    enc = tf.expand_dims(
+        tf.concat([tf.sin(pos / denom), tf.cos(pos / denom)], 1), 0)
+    return tf.tile(enc, [tf.shape(inputs)[0], 1, 1])
+
+
+def layer_norm(inputs, epsilon=1e-8):
+    mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+    normalized = (inputs - mean) / (tf.sqrt(variance + epsilon))
+    params_shape = inputs.get_shape()[-1:]
+    gamma = tf.get_variable('gamma', params_shape,
+                            tf.float32, tf.ones_initializer())
+    beta = tf.get_variable('beta', params_shape,
+                           tf.float32, tf.zeros_initializer())
+    return gamma * normalized + beta
+
+
+def cnn_block(x, dilation_rate, pad_sz, hidden_dim, kernel_size):
+    x = layer_norm(x)
+    pad = tf.zeros([tf.shape(x)[0], pad_sz, hidden_dim])
+    x = tf.layers.conv1d(inputs=tf.concat([pad, x, pad], 1),
+                         filters=hidden_dim,
+                         kernel_size=kernel_size,
+                         dilation_rate=dilation_rate)
+    x = x[:, :-pad_sz, :]
+    x = tf.nn.relu(x)
+    return x
+# GLU
+
+
+def gated_linear_units(inputs):
+    input_shape = inputs.get_shape().as_list()
+    assert len(input_shape) == 3
+    input_pass = inputs[:, :, 0:int(input_shape[2]/2)]
+    input_gate = inputs[:, :, int(input_shape[2]/2):]
+    input_gate = tf.sigmoid(input_gate)
+    return tf.multiply(input_pass, input_gate)
+
 # Model for Machine Translation
+
+
 class Seq2SeqModel(object):
     def __init__(self, vocab_size_en, vocab_size_vi, word_embedding_en, word_embedding_vi, input_len, output_len, params, train=True):
         # Get the vocab size
@@ -207,6 +321,7 @@ class Seq2SeqModel(object):
         self.keep_prob = params['keep_prob']
         self.beam_width = params['beam_width']
 
+        self.kernel_size = params['kernel_size']
         # Using BasicLSTMCell as a cell unit
         self.cell = tf.nn.rnn_cell.LSTMCell
 
@@ -241,6 +356,41 @@ class Seq2SeqModel(object):
                 self.embeddings_en, self.inputSeq)
             decoder_input_emb = tf.nn.embedding_lookup(
                 self.embeddings_vi, self.decoder_input)
+
+            #layer = 0
+            input_emb += position_encoding(input_emb)
+            for i in range(self.num_layers):
+                next_layer = input_emb
+                dilation_rate = 2 ** i
+                pad_sz = (self.kernel_size - 1) * dilation_rate
+                with tf.variable_scope('block_%d' % i, reuse=tf.AUTO_REUSE):
+                    # input_emb += cnn_block(input_emb, dilation_rate,
+                    #                              pad_sz, 100, self.kernel_size)
+                    #layer += gated_linear_units(input_emb)
+                    next_layer = conv1d_weightnorm(inputs=next_layer, layer_idx=i, out_dim=100*2,
+                                                   kernel_size=self.kernel_size, padding="SAME", dropout=0.9, var_scope_name="conv_layer_"+str(i))
+                    next_layer = gated_linear_units(next_layer)
+                    input_emb = (next_layer + input_emb) * tf.sqrt(0.5)
+            #input_emb = gated_linear_units(input_emb)
+
+            #input_emb = layer
+            #print("emb: ",input_emb)
+            '''
+            print("emb1: ", input_emb)
+            #CNN encoder
+            input_emb += position_encoding(input_emb)
+            
+            for i in range(self.num_layers): 
+                next_layer = input_emb
+                dilation_rate = 2 ** i
+                pad_sz = (self.kernel_size - 1) * dilation_rate 
+                with tf.variable_scope('block',reuse=tf.AUTO_REUSE):
+                next_layer = conv1d_weightnorm(inputs=next_layer, layer_idx=i, out_dim=100*2, kernel_size=self.kernel_size, padding="SAME", dropout=0.9, var_scope_name="conv_layer_"+str(i))
+                next_layer = gated_linear_units(next_layer)
+                input_emb = (next_layer + input_emb) * tf.sqrt(0.5)
+
+            print("ebm2: ", input_emb)
+            '''
 
             # Convert from batch_size*seq_len*embedding to seq_len*batch_size*embedding to feed data with timestep
             # But, we need to set time_major=True during Training
@@ -363,7 +513,8 @@ class Seq2SeqModel(object):
                 outputs = tf.transpose(outputs.predicted_ids, perm=[1, 2, 0])
 
                 # Take the first beam (best result) as Decoder ouput
-                self.decoder_outputs = outputs[:, 0, :]
+                # self.decoder_outputs=outputs[:,0,:]
+                self.decoder_outputs = outputs
 
         with tf.name_scope("optimization"):
             # Used for Training mode only
@@ -380,21 +531,22 @@ class Seq2SeqModel(object):
                 gradients = optimizer.compute_gradients(self.loss)
                 # Apply Gradient Clipping
                 gradients_clipping = [(tf.clip_by_value(
-                    grad, clip_value_min=-5., clip_value_max=5.), var) for grad, var in gradients if grad is not None]
+                    grad, clip_value_min=-1., clip_value_max=1.), var) for grad, var in gradients if grad is not None]
 
                 # Apply gradients to variables
                 self.train_update = optimizer.apply_gradients(
                     gradients_clipping, global_step=self.global_step)
+                #self.train_update = optimizer.apply_gradients(gradients, global_step=self.global_step)
 
 
 # define hyparamater
 params = dict()
-params['num_layers'] = 1
+params['num_layers'] = 2
 params['num_hiddens'] = 512
 params['learning_rate'] = 0.001
 params['keep_prob'] = 0.85
 params['beam_width'] = 10
-
+params['kernel_size'] = 3
 # Path of the saved model
 checkpoint = "NMT.ckpt"
 
@@ -424,6 +576,17 @@ with sess.as_default():
     # Load the value of variables in saved model
         saver = tf.compat.v1.train.Saver(tf.compat.v1.global_variables())
         saver.restore(sess, checkpoint)
+
+
+def getSeq(dic):
+    index = 0
+    maxL = model.score(dic[0])
+    for i in range(1, 10):
+        temp = model.score(dic[i])
+        if maxL < temp:
+            maxL = temp
+            index = i
+    return dic[index]
 
 
 def predict(inputSeq):
@@ -465,22 +628,26 @@ def predict(inputSeq):
             decoder_outputs = sess.run(
                 loaded_model.decoder_outputs, feed_dict=valid_feed_dict)
 
-            # Convert from sequence of int to actual sentence
-            output_titles = []
             # Loop through each seq in decoder_outputs
-            for out_seq in decoder_outputs:
-                out_sent = list()
-                for word_int in out_seq:
-                    # Convert int to word
-                    word = int2word_vietnamese[word_int]
-                    # Stop converting when it reach to the end of ouput sentence
-                    if word == "</s>":
-                        break
-                    else:
-                        out_sent.append(word)
-                # Combine list of word to sentence and add this sentence to output_titles
-                output_titles.append(" ".join(out_sent))
-    return output_titles
+            listOut = []
+            for out10_seq in decoder_outputs:
+                listSeq = []
+                for out_seq in out10_seq:
+                    out_sent = list()
+                    for word_int in out_seq:
+                        word = int2word_vietnamese[word_int]
+                        if word == "</s>":
+                            break
+                        else:
+                            out_sent.append(word)
+                    listSeq.append(" ".join(out_sent))
+                listOut.append(listSeq)
+
+            listOutput = []
+            # Using model language_moel
+            for listSeq in listOut:
+                listOutput.append(listSeq)
+    return listOutput
 
 
 @app.route('/')
